278ffb55e5171b4ad98c070b157654bc9e736e6e
山木 <750154432@qq.com>
2024-04-24 17:36:00 +0800

    [feat]（code improve）：add logic to get table name from annotation.

--- koto-k2-compiler-plugin/src/main/kotlin/com/kotoframework/plugins/utils/updateClause/SetUpdateClauseTableName.kt
+++ koto-k2-compiler-plugin/src/main/kotlin/com/kotoframework/plugins/utils/updateClause/SetUpdateClauseTableName.kt
@@ -4,15 +4,16 @@ import org.jetbrains.kotlin.backend.common.extensions.FirIncompatiblePluginAPI
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
-import org.jetbrains.kotlin.ir.backend.js.utils.valueArguments
 import org.jetbrains.kotlin.ir.builders.irCall
 import org.jetbrains.kotlin.ir.builders.irString
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
-import org.jetbrains.kotlin.ir.expressions.IrFunctionExpression
-import org.jetbrains.kotlin.ir.types.toKotlinType
-import org.jetbrains.kotlin.js.descriptorUtils.getKotlinTypeFqName
+import org.jetbrains.kotlin.ir.types.IrSimpleType
+import org.jetbrains.kotlin.ir.types.getClass
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.resolve.descriptorUtil.classId
+import org.jetbrains.kotlin.types.checker.SimpleClassicTypeSystemContext.getArguments
 
 @OptIn(FirIncompatiblePluginAPI::class, ObsoleteDescriptorBasedAPI::class)
 fun setUpdateClauseTableName(pluginContext: IrPluginContext, expression: IrCall): IrExpression {
@@ -21,15 +22,19 @@ fun setUpdateClauseTableName(pluginContext: IrPluginContext, expression: IrCall)
         pluginContext.referenceFunctions(FqName("com.kotoframework.orm.update.setUpdateClauseTableName"))
             .first(),
     ).apply {
+        val irClass = (expression.type.getArguments().first() as IrSimpleType).getClass()!!
+        val annotations = irClass.annotations
+        val tableAnnotation =
+            annotations.firstOrNull { it.symbol.descriptor.containingDeclaration.classId == ClassId.fromString("com/kotoframework/annotations/Table") }
         putValueArgument(0, expression)
         putValueArgument(
-            1, irBuilder.irCall(
+            1,
+            tableAnnotation?.getValueArgument(0) ?: irBuilder.irCall(
                 pluginContext.referenceFunctions(FqName("com.kotoframework.utils.tableK2db")).first()
             ).apply {
-                val name = (expression.valueArguments.first() as IrFunctionExpression).type.toKotlinType().arguments.first().type.arguments.first().type.getKotlinTypeFqName(false)
                 putValueArgument(0,
                     irBuilder.irString(
-                        name
+                        irClass.name.asString()
                     )
                 )
             }

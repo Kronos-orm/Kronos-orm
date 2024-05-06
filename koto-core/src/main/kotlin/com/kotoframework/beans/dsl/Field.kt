2ca5028df92006260122948fd9d93cc5ad554fce
yf <1661264104@qq.com>
2024-05-06 09:12:33 +0800

    [fix] 给field加hashcode和equals

--- koto-core/src/main/kotlin/com/kotoframework/beans/dsl/Field.kt
+++ koto-core/src/main/kotlin/com/kotoframework/beans/dsl/Field.kt
@@ -9,4 +9,22 @@ class Field(
     override fun toString(): String {
         return name
     }
+    fun quotedColumnName(): String = "`$columnName`"
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Field
+
+        if (columnName != other.columnName) return false
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = columnName.hashCode()
+        result = 31 * result + name.hashCode()
+        return result
+    }
 }

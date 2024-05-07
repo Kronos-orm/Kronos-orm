c0dd9f87837921da839eec405e6263ed24dc1013
ousc <sundaiyue@foxmail.com>
2024-05-07 10:47:17 +0800

    [feat][Kronos]：将 `defaultLogger`、`defaultNoValueStrategy`、`defaultSerializeResolver` 修改为 `loggerType`、`noValueStrategy`、`serializeResolver` [feat][KronosCommonStrategy]：新增 `updateTimeStrategy`、`createTimeStrategy`、`logicDeleteStrategy` [feat][TbUser]：为 `updateTime` 增加时间格式 [fix][KotoLoggerApp]：修复 `Kronos.defaultLoggerType` 为 `Kronos.loggerType`

---
+++ kronos-core/src/main/kotlin/com/kotlinorm/annotations/CreateTime.kt
@@ -0,0 +1,5 @@
+package com.kotlinorm.annotations
+
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CreateTime(val timeFormat: String = "yyyy-MM-dd HH:mm:ss", val enable: Boolean = true)

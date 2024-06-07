a4da25c3a38f0f3c9261e9fd2ee04ea2c74f8a0a
山木 <750154432@qq.com>
2024-06-07 11:33:49 +0800

    - [feat] (Build): Update build.gradle file

---
+++ kronos-testing/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.dokka") apply false
+    id("com.vanniktech.maven.publish") apply false
+//    id("com.kotlinorm.kronos-compiler-plugin")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation(project(":kronos-core"))
+    testImplementation(project(":kronos-logging"))
+    testImplementation(project(":kronos-jvm-driver-wrapper"))
+    testImplementation("commons-dbcp:commons-dbcp:1.4")
+    testImplementation("com.mysql:mysql-connector-j:8.4.0")
+    testImplementation("com.google.code.gson:gson:2.11.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}

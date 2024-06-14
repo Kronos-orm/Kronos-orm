3f4e5dcfa8c47eeae1706a22cb94738e66708870
ousc <750154432@qq.com>
2024-06-14 12:01:15 +0800

    [feat] (CascadeDeleteClause): Improve the build method to support generics of KPojo type, and add generateReferenceUpdateSql private method to generate reference update SQL

--- kronos-core/src/main/kotlin/com/kotlinorm/orm/delete/CascadeDeleteClause.kt
+++ kronos-core/src/main/kotlin/com/kotlinorm/orm/delete/CascadeDeleteClause.kt
@@ -1,7 +1,11 @@
 package com.kotlinorm.orm.delete
 
 import com.kotlinorm.beans.dsl.Criteria
+import com.kotlinorm.beans.dsl.KPojo
+import com.kotlinorm.beans.dsl.KReference
 import com.kotlinorm.beans.task.KronosAtomicActionTask
+import com.kotlinorm.enums.KOperationType
+import kotlin.reflect.full.memberProperties
 
 /**
  * Used to build a cascade delete clause.
@@ -16,7 +20,25 @@ object CascadeDeleteClause {
      * @param condition The condition to be met.
      * @return The list of atomic tasks.
      */
-    fun <T> build(pojo: T, condition: Criteria?): Array<KronosAtomicActionTask> {
-        TODO()
+    fun <T : KPojo> build(pojo: T, condition: Criteria?): Array<KronosAtomicActionTask> {
+        val allReferences = pojo.kronosColumns().mapNotNull { it.reference }
+        return allReferences.map { ref ->
+            val propVal = pojo::class.memberProperties.find { it.name == ref.propName }!!.getter.call(pojo)
+            if (propVal is Iterable<*>) {
+                propVal.map {
+                    generateReferenceUpdateSql(it as KPojo, ref, condition)
+                }
+            } else {
+                listOf(generateReferenceUpdateSql(propVal as KPojo, ref, condition))
+            }
+        }.flatten().toTypedArray()
+    }
+
+    private fun <K : KPojo> generateReferenceUpdateSql(
+        pojo: K,
+        reference: KReference,
+        condition: Criteria?
+    ): KronosAtomicActionTask {
+        return KronosAtomicActionTask("", mapOf(), KOperationType.UPDATE)
     }
 }

02404825523d98530e6f22277e7380a5583410a3
ousc <750154432@qq.com>
2024-06-27 14:33:12 +0800

    - [fix]（工具函数）：更新在 SQL 语句中跳过字符的逻辑，修复了缺少反引号的问题 - [feat]（解析器）：添加对包含方括号的参数的解析功能

--- kronos-core/src/main/kotlin/com/kotlinorm/beans/dsw/ParsedSql.kt
+++ kronos-core/src/main/kotlin/com/kotlinorm/beans/dsw/ParsedSql.kt
@@ -46,44 +46,26 @@ class ParsedSql(private var originalSql: String = "") {
         parameterIndexes.add(intArrayOf(startIndex, endIndex))
     }
 
-    /**
-     * Return all the parameters (bind variables) in the parsed SQL statement.
-     * Repeated occurrences of the same parameter name are included here.
-     */
-    fun getParameterNames(): List<String> {
-        return parameterNames
-    }
-
-    /**
-     * Return the parameter indexes for the specified parameter.
-     * @param parameterPosition the position of the parameter
-     * (as index in the parameter names List)
-     * @return the start index and end index, combined into
-     * a int array of length 2
-     */
-    fun getParameterIndexes(parameterPosition: Int): IntArray {
-        return parameterIndexes[parameterPosition]
-    }
-
     var paramMap: Map<String, Any?> = mapOf()
 
-    val jdbcSql: String
-        get() {
-            var sql = originalSql
-            for (parameterName in parameterNames) {
-                sql = sql.replace(":$parameterName", "?")
-            }
-            return sql
-        }
+    var jdbcSql: String = ""
+    val jdbcParamList: MutableList<Any?> = mutableListOf()
 
-    val jdbcParamList: List<Any?>
-        get() {
-            val paramList: MutableList<Any?> = mutableListOf()
-            for (parameterName in parameterNames) {
-                paramList.add(paramMap[parameterName])
+    fun executeParse() {
+        var sql = originalSql
+        for (parameterName in parameterNames) {
+            sql = if (paramMap[parameterName] is Collection<*>) {
+                jdbcParamList.addAll(paramMap[parameterName] as Collection<*>)
+                sql.replace(
+                    ":$parameterName",
+                    (paramMap[parameterName] as Collection<*>).joinToString(",") { _ -> "?" })
+            } else {
+                jdbcParamList.add(paramMap[parameterName])
+                sql.replace(":$parameterName", "?")
             }
-            return paramList
         }
+        jdbcSql = sql
+    }
 
     /**
      * Exposes the original SQL String.

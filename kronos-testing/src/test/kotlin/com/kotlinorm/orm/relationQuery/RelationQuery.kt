18e6d700e8459a8f502a2ed687f6cb9adf32de2c
lujieyao <2456416562@qq.com>
2024-06-27 15:48:47 +0800

    [feat](关系查询):添加新的测试用例，包括查询学生、班级和学校的关系，级联插入和更新操作。

--- kronos-testing/src/test/kotlin/com/kotlinorm/orm/relationQuery/RelationQuery.kt
+++ kronos-testing/src/test/kotlin/com/kotlinorm/orm/relationQuery/RelationQuery.kt
@@ -6,10 +6,13 @@ import com.kotlinorm.KronosBasicWrapper
 import com.kotlinorm.beans.namingStrategy.LineHumpNamingStrategy
 import com.kotlinorm.orm.database.table
 import com.kotlinorm.orm.delete.delete
+import com.kotlinorm.orm.insert.InsertClause.Companion.execute
 import com.kotlinorm.orm.insert.insert
 import com.kotlinorm.orm.relationQuery.oneToMany.GroupClass
 import com.kotlinorm.orm.relationQuery.oneToMany.School
 import com.kotlinorm.orm.relationQuery.oneToMany.Student
+import com.kotlinorm.orm.select.select
+import com.kotlinorm.orm.update.update
 import com.kotlinorm.orm.utils.GsonResolver
 import org.apache.commons.dbcp.BasicDataSource
 import org.junit.jupiter.api.Test
@@ -20,7 +23,7 @@ class RelationQuery {
         driverClassName = "com.mysql.cj.jdbc.Driver"
         url = "jdbc:mysql://localhost:3306/test"
         username = "root"
-        password = "Leinbo2103221541@"
+        password = "rootroot"
     }
 
     init {
@@ -30,6 +33,13 @@ class RelationQuery {
             dataSource = { KronosBasicWrapper(ds) }
             serializeResolver = GsonResolver
         }
+    }
+
+    @Test
+    fun a() {
+        val student = Student().select().where { it.id == 1 && it.groupClass!!.groupNo.isNull }.queryOne()
+        val groupClass = student.groupClass
+        val students = groupClass!!.students
     }
 
     @Test
@@ -150,5 +160,51 @@ class RelationQuery {
                     "LIMIT 1) = 0",
             task.component3()[0].sql
         )
+    }
+
+    @Test
+    fun testMultipleCascade() {
+        val school = School(1 , name = "aaa")
+
+        val groupClasses = (1..10).map { cId ->
+            val c = GroupClass(cId, schoolName = "aaa")
+
+            val students = (1..10).map { stuId ->
+                Student(stuId, groupClassId = cId).apply {
+                    this.groupClass = c
+                }
+            }
+
+            c.apply {
+                c.school = school
+                c.students = students
+            }
+        }
+
+        school.groupClass = groupClasses
+        val tasks = school.delete().where().build()
+        println(tasks.component3().size)
+    }
+
+    @Test
+    fun testUpdate() {
+
+        dataSource.table.dropTable<School>()
+        dataSource.table.dropTable<GroupClass>()
+        dataSource.table.dropTable<Student>()
+        dataSource.table.createTable<School>()
+        dataSource.table.createTable<GroupClass>()
+        dataSource.table.createTable<Student>()
+
+        val groupClass = GroupClass(1)
+        val stus = (1..10).map {
+            Student(it, groupClassId = 1)
+        }
+        groupClass.students = stus
+        groupClass.insert().execute()
+
+        val task = groupClass.update().set { it.id = 999 }.by { it.id }.build()
+        val (affectRows, lastInsertId) = task.execute()
+        println(lastInsertId)
     }
 }

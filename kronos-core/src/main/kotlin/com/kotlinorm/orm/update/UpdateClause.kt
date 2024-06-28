bd8445ae7ffd4110cef399bd6d246ba3a4383008
lujieyao <2456416562@qq.com>
2024-06-28 14:30:50 +0800

    [fix]（ORM）：修复更新逻辑策略未更新的问题。

--- kronos-core/src/main/kotlin/com/kotlinorm/orm/update/UpdateClause.kt
+++ kronos-core/src/main/kotlin/com/kotlinorm/orm/update/UpdateClause.kt
@@ -161,9 +161,6 @@ class UpdateClause<T : KPojo>(
      */
     fun build(): KronosActionTask {
 
-        updateTimeStrategy.enabled = this.updateTimeStrategy.enabled
-        logicDeleteStrategy.enabled = this.logicDeleteStrategy.enabled
-
         // 处理字段更新逻辑，如果isExcept为true，则移除特定字段，否则更新所有字段
         if (isExcept) {
             // 移除指定字段并处理"create_time"字段的特殊情况

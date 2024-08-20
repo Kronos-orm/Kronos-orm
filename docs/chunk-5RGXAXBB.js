e34c113caa0fc86a76ad3de3fba61c59855be3c0
ousc <750154432@qq.com>
2024-08-20 17:30:35 +0800

    [feat] update website

--- docs/chunk-5RGXAXBB.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-ODN5LVDJ.js";var t=[{path:"",redirectTo:"quick-start",pathMatch:"full"},{path:"",title:"\u5FEB\u901F\u5F00\u59CB",children:[{path:"quick-start",loadChildren:()=>import("./chunk-QPPPW53Q.js")},{path:"welcome",loadChildren:()=>import("./chunk-IJHOEVSO.js")}]}],a=t;export{a as default};

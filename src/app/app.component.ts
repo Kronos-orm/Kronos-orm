65f8076baa7ba1d41fad6036065144aaefefcab8
ousc <750154432@qq.com>
2024-08-27 14:05:18 +0800

    [feat](页面交互): 实现首次进入时的动画展示与交互功能
    - 引入`ngx-typed-writer`库，用于在首次进入页面时展示代码动画效果。
    - 在首次加载时使用`@defer`和`p-button`组件控制动画流程，动画结束后显示路由内容。
    - 添加了字体`JetBrains Mono`并在样式中使用，以确保代码展示的统一性。
    - 实现了多个动画和字体特效，提升用户首次加载页面的体验。
    
    [fix](依赖): 移除未使用的`echarts`相关依赖项
    
    - 删除了`echarts`及其相关的`zrender`库依赖，优化项目依赖管理，减小包体积。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,32 @@ import {SharedModule} from "./shared.module";
 import {PrimeNGConfig} from 'primeng/api';
 import {AppService} from "./app.service";
 import {TranslocoService} from "@jsverse/transloco";
+import {NgxTypedWriterModule} from "ngx-typed-writer";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [SharedModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  providers: [AppService]
+    selector: 'app-root',
+    standalone: true,
+    imports: [SharedModule, NgxTypedWriterModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    providers: [AppService]
 })
 export class AppComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig, private translocoService: TranslocoService, private appService: AppService) {
-  }
+    constructor(private primengConfig: PrimeNGConfig, private translocoService: TranslocoService, private appService: AppService) {
+    }
 
-  title = "Kronos"
+    title = "Kronos"
 
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-    this.translocoService.setActiveLang(this.appService.language);
-  }
+    ngOnInit() {
+        this.primengConfig.ripple = true;
+        this.translocoService.setActiveLang(this.appService.language);
+    }
+
+    firstEntry = true;
+    codes = [
+        '.<span class=\'code-green\'>insert()</span>.<span class=\'code-green\'>execute()</span>',
+        '.<span class=\'code-green\'>delete()</span><br/>.<span class=\'code-green\'>where{ </span><span class=\'code-red\'>it.id </span>== 1<span class=\'code-green\'> }</span><br/>.<span class=\'code-green\'>execute()</span>',
+        '.<span class=\'code-green\'>select()</span><br/>.<span class=\'code-green\'>where{ </span><span class=\'code-red\'>it.id </span>== 1<span class=\'code-green\'> }</span><br/>.<span class=\'code-green\'>queryList()</span>',
+        '.<span class=\'code-green\'>update()</span><br/>.<span class=\'code-green\'>set{ </span><span class=\'code-red\'>it.name </span>== \"name\"<span class=\'code-green\'> }</span><br/>.<span class=\'code-green\'>where{ </span><span class=\'code-red\'>it.id </span>== 1<span class=\'code-green\'> }</span><br/>.<span class=\'code-green\'>execute()</span>',
+    ]
 }

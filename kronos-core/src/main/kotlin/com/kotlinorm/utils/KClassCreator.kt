8be5c0fbf0a76134b2bd66e6186c1cf495c73ed8
ousc <750154432@qq.com>
2024-11-19 11:54:49 +0800

    - [fix] (instantiation): optimize exception messages to improve readability and user guidance

--- kronos-core/src/main/kotlin/com/kotlinorm/utils/KClassCreator.kt
+++ kronos-core/src/main/kotlin/com/kotlinorm/utils/KClassCreator.kt
@@ -9,6 +9,15 @@ var kClassCreatorCustom: (KClass<out KPojo>) -> KPojo? = { null }
 @Suppress("UNCHECKED_CAST")
 fun <T : KPojo> KClass<T>.createInstance(): T {
     return kClassCreator(this) as T? ?: kClassCreatorCustom(this) as T? ?: throw NullPointerException(
-        "KClass ${this.simpleName} instantiation failed \n1.Please check if the data class has a no-argument constructor. \n2.if you did not add `Kronos.init{}` to your code, please add it" + " to the code, this is a necessary step to use the ORM framework \n3.if you have added it, Maybe you're referencing some third-party library and using a KPojo class defined by them，" + "please add a custom instantiation method to the `kClassCreatorCustom` like this: \nkClassCreatorCustom = { kClass -> \n\twhen (kClass) {\n\t\tYourKPojo::class -> YourKPojo()\n\t\telse -> null\n\t}\n}"
+        "KClass ${this.simpleName} instantiation failed \n" +
+                "1.Please check if the data class has a no-argument constructor. \n" +
+                "2.if you did not add `Kronos.init{}` to your code, please add it  to the code, " +
+                "this is a necessary step to use the ORM framework \n" +
+                "3.if you have added it, Maybe you're referencing some third-party library and using a KPojo class defined by them，" +
+                "please add a custom instantiation method to the `kClassCreatorCustom` like this: \n" +
+                "kClassCreatorCustom = { kClass -> \n" +
+                "\twhen (kClass) {\n" +
+                "\t\tYourKPojo::class -> YourKPojo()\n" +
+                "\t\telse -> null\n\t}\n}"
     )
 }

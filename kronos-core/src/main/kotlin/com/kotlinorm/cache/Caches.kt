36d8ebd4c9ecc24005b55c48d9b184a7d62639a0
山木 <750154432@qq.com>
2025-04-26 01:17:17 +0800

    - [feat] (Cache): Add named SQL cache to optimize the parsing process

--- kronos-core/src/main/kotlin/com/kotlinorm/cache/Caches.kt
+++ kronos-core/src/main/kotlin/com/kotlinorm/cache/Caches.kt
@@ -1,19 +1,18 @@
 package com.kotlinorm.cache
 
 import com.kotlinorm.beans.dsl.Field
+import com.kotlinorm.beans.parser.NamedParameterUtils.ParameterHolder
+import com.kotlinorm.beans.parser.ParsedSql
 import com.kotlinorm.interfaces.KPojo
 import com.kotlinorm.utils.LRUCache
 import com.kotlinorm.utils.createInstance
 import kotlin.reflect.KClass
 
-val fieldsMapCache = LRUCache<KClass<KPojo>, Map<String, Field>> {
-    it.createInstance().kronosColumns().let { instance ->
-        instance.associate {
-            it.name to it
-        } + instance.associate {
-            it.columnName to it
-        }
+val fieldsMapCache = LRUCache<KClass<KPojo>, Map<String, Field>> { kClass->
+    kClass.createInstance().kronosColumns().let { instance ->
+        instance.associateBy { it.name } + instance.associateBy { it.columnName }
     }
 }
 
 val insertSqlCache = LRUCache<Pair<KClass<KPojo>, Boolean>, String>()
+val namedSqlCache = LRUCache<String, ParsedSql>()

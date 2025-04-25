cd6db332f7cbb28a580f99bb49faa19e345ee96d
山木 <750154432@qq.com>
2025-04-26 01:54:14 +0800

    - [feat]（TransformerManager）：Add method for logging out value converter - [fix]（NamedParameterUtils）：Correct the operation of reducing the count of unnamed parameters - [feat]（EnumValueTransformTest）：Log out test converter in the test

--- kronos-core/src/main/kotlin/com/kotlinorm/beans/parser/NamedParameterUtils.kt
+++ kronos-core/src/main/kotlin/com/kotlinorm/beans/parser/NamedParameterUtils.kt
@@ -353,6 +353,7 @@ object NamedParameterUtils {
                 actualSql.append('?')
             }
             lastIndex = endIndex
+            parsedSql.unnamedParameterCount --
         }
         actualSql.append(originalSql, lastIndex, originalSql.length)
         return actualSql.toString()

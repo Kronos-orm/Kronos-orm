2caa6ae594728f6b16a04dea142978f3355b6ad4
山木 <750154432@qq.com>
2025-05-04 23:36:19 +0800

    - [fix] (Test): Fixed the method of obtaining the last inserted ID when inserting a user - [feat] (Test): Added table clearing operation in the test to ensure data consistency

--- kronos-testing/src/test/kotlin/com/kotlinorm/database/TableOperationMysql.kt
+++ kronos-testing/src/test/kotlin/com/kotlinorm/database/TableOperationMysql.kt
@@ -12,6 +12,7 @@ import com.kotlinorm.enums.DBType
 import com.kotlinorm.orm.database.table
 import com.kotlinorm.orm.insert.insert
 import com.kotlinorm.plugins.LastInsertIdPlugin.lastInsertId
+import com.kotlinorm.plugins.LastInsertIdPlugin.withId
 import org.apache.commons.dbcp2.BasicDataSource
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -168,12 +169,13 @@ class TableOperationMysql {
     @Test
     fun testLastInsertId() {
         dataSource.table.syncTable(user)
+        dataSource.table.truncateTable(user)
         wrapper.execute("ALTER TABLE tb_user AUTO_INCREMENT = 528")
         val newUser = MysqlUser(
             username = "yf",
             gender = 93
         )
-        val lastInsertId = newUser.insert().execute().lastInsertId
+        val lastInsertId = newUser.insert().withId().execute().lastInsertId
         assertEquals(528, lastInsertId, "自增主键值应为528")
     }
 

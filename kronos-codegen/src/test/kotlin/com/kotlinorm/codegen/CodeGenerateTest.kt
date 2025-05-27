7b3097d2d27fe597fd646acf93a7cdf18833d0c1
山木 <750154432@qq.com>
2025-05-28 01:57:08 +0800

    - [fix] (Test): Fixed database connection configuration to prevent errors caused by empty environment variables

--- kronos-codegen/src/test/kotlin/com/kotlinorm/codegen/CodeGenerateTest.kt
+++ kronos-codegen/src/test/kotlin/com/kotlinorm/codegen/CodeGenerateTest.kt
@@ -69,8 +69,8 @@ class CodeGenerateTest {
                 dataSourceClassName = "org.apache.commons.dbcp2.BasicDataSource"
                 wrapperClassName = "com.kotlinorm.KronosBasicWrapper"
                 url = "jdbc:mysql://localhost:3306/kronos_testing?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=Asia/Shanghai&allowMultiQueries=true&allowPublicKeyRetrieval=true&useServerPrepStmts=false&rewriteBatchedStatements=true"
-                username = "${System.getenv("db.username").orEmpty()}"
-                password = "${System.getenv("db.password").orEmpty()}"
+                username = "root"
+                password = ""
                 driverClassName = "com.mysql.cj.jdbc.Driver"
                 initialSize = 5
                 maxActive = 10

b0fdd0a2aeaba7c41a15d649a50168e3fa689a02
山木 <750154432@qq.com>
2025-05-28 01:38:11 +0800

    - [feat](CI/CD): Add MySQL settings and connection test steps

--- kronos-codegen/src/test/kotlin/com/kotlinorm/codegen/CodeGenerateTest.kt
+++ kronos-codegen/src/test/kotlin/com/kotlinorm/codegen/CodeGenerateTest.kt
@@ -69,8 +69,8 @@ class CodeGenerateTest {
                 dataSourceClassName = "org.apache.commons.dbcp2.BasicDataSource"
                 wrapperClassName = "com.kotlinorm.KronosBasicWrapper"
                 url = "jdbc:mysql://localhost:3306/kronos_testing?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC&useSSL=false&useServerPrepStmts=true&rewriteBatchedStatements=true"
-                username = "root"
-                password = "******"
+                username = ""
+                password = ""
                 driverClassName = "com.mysql.cj.jdbc.Driver"
                 initialSize = 5
                 maxActive = 10

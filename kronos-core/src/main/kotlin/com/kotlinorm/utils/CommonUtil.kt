88f21f156bebcb7f5bca9b577a0512c586833964
lujieyao <2456416562@qq.com>
2025-06-04 15:59:19 +0800

    [fix] (Database Adaptation): Correct the boolean value conversion logic for the Postgres database

--- kronos-core/src/main/kotlin/com/kotlinorm/utils/CommonUtil.kt
+++ kronos-core/src/main/kotlin/com/kotlinorm/utils/CommonUtil.kt
@@ -196,7 +196,7 @@ fun getDefaultBoolean(
     boolean: Boolean
 ): Any {
     return when (wrapper.dbType) {
-        DBType.Postgres -> false
+        DBType.Postgres -> true
         else -> {
             if (boolean) 1 else 0
         }

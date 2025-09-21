4838d3f4fdfb13c1d75680249d50f3cce36d3120
山木 <750154432@qq.com>
2025-09-21 22:27:34 +0800

    [refactor] Enhance UnionClause for improved readability and maintainability by standardizing code formatting and optimizing field mapping logic

--- kronos-core/src/main/kotlin/com/kotlinorm/orm/union/UnionClause.kt
+++ kronos-core/src/main/kotlin/com/kotlinorm/orm/union/UnionClause.kt
@@ -1,22 +1,20 @@
 /**
  * Copyright 2022-2025 kronos-orm
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * ```
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * ```
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package com.kotlinorm.orm.union
 
 import com.kotlinorm.beans.task.KronosQueryTask
+import com.kotlinorm.cache.kPojoAllFieldsCache
 import com.kotlinorm.interfaces.KPojo
 import com.kotlinorm.interfaces.KronosDataSourceWrapper
 import com.kotlinorm.orm.select.SelectClause
@@ -41,15 +39,18 @@ open class UnionClause(tasks: List<SelectClause<out KPojo>>) {
     }
 
     private fun executeQuery(
-        queryFunction: (KronosQueryTask) -> List<Map<String, Any?>>
+            queryFunction: (KronosQueryTask) -> List<Map<String, Any?>>
     ): List<Map<String, Any?>> {
         prepareTasks()
         val safeKeyList = getUniqueTask(KeyCounter())
 
-        val resultMap = safeKeyList.mapIndexed { index, safeKey ->
-            val result = queryFunction(builtTasks[index])
-            safeKey to result
-        }.toMap()
+        val resultMap =
+                safeKeyList
+                        .mapIndexed { index, safeKey ->
+                            val result = queryFunction(builtTasks[index])
+                            safeKey to result
+                        }
+                        .toMap()
 
         return if (resultMap.isEmpty()) emptyList() else union(resultMap)
     }
@@ -60,21 +61,51 @@ open class UnionClause(tasks: List<SelectClause<out KPojo>>) {
 
     private fun getUniqueTask(keyCounters: KeyCounter): List<String> {
         val fieldCountMap = mutableMapOf<String, Int>()
-        safeKeyList = tasks.map { task ->
-            val safeKey = "${task.pojo::class.simpleName}_${keyCounters.getNext(task.pojo::class.simpleName.toString())}"
-            task.selectFields.forEach { field ->
-                fieldCountMap[field.name] = fieldCountMap.getOrDefault(field.name, 0) + 1
-            }
-            safeKey
-        }
+        safeKeyList =
+                tasks.map { task ->
+                    val safeKey =
+                            "${task.pojo::class.simpleName}_${keyCounters.getNext(task.pojo::class.simpleName.toString())}"
+                    val fieldsToUse =
+                            if (task.selectFields.isEmpty()) {
+                                // 如果selectFields为空，使用所有字段
+                                kPojoAllFieldsCache[task.pojo.kClass()]!!
+                            } else {
+                                task.selectFields
+                            }
+                    fieldsToUse.forEach { field ->
+                        fieldCountMap[field.name] = fieldCountMap.getOrDefault(field.name, 0) + 1
+                    }
+                    safeKey
+                }
 
         fieldNameMapping.clear()
         tasks.forEachIndexed { index, task ->
             val safeKey = safeKeyList[index]
-            fieldNameMapping[safeKey] = task.selectFields.associate { field ->
-                val fieldName = field.name
-                fieldName to if (fieldCountMap[fieldName]!! > 1) "$safeKey${capitalizeFirstLetter(fieldName)}" else fieldName
-            }.toMutableMap()
+            val className = task.pojo::class.simpleName
+            // 计算suffix：同一个类的第二个实例及以后才有suffix
+            val sameClassCount = tasks.take(index).count { it.pojo::class.simpleName == className }
+            val suffix = if (sameClassCount > 0) "@$sameClassCount" else ""
+            val fieldsToUse =
+                    if (task.selectFields.isEmpty()) {
+                        // 如果selectFields为空，使用所有字段
+                        kPojoAllFieldsCache[task.pojo.kClass()]!!
+                    } else {
+                        task.selectFields
+                    }
+            val mapping =
+                    fieldsToUse
+                            .associate { field ->
+                                val fieldName = field.name
+                                // 只有当字段名冲突时才加前缀
+                                if (fieldCountMap[fieldName]!! > 1) {
+                                    fieldName to
+                                            "$className$suffix${capitalizeFirstLetter(fieldName)}"
+                                } else {
+                                    fieldName to fieldName
+                                }
+                            }
+                            .toMutableMap()
+            fieldNameMapping[safeKey] = mapping
         }
         return safeKeyList
     }
@@ -82,18 +113,24 @@ open class UnionClause(tasks: List<SelectClause<out KPojo>>) {
     private fun union(resultMap: Map<String, List<Map<String, Any?>>>): List<Map<String, Any?>> {
         val maxRowNum = resultMap.values.maxOfOrNull { it.size } ?: 0
 
-        val allFields = resultMap.flatMap { (safeKey, list) ->
-            list.flatMap { it.keys.map { key -> fieldNameMapping[safeKey]!![key]!! } }
-        }.toSet()
+        val allFields =
+                resultMap
+                        .flatMap { (safeKey, list) ->
+                            list.flatMap {
+                                it.keys.map { key -> fieldNameMapping[safeKey]?.get(key) ?: key }
+                            }
+                        }
+                        .toSet()
 
         return List(maxRowNum) { index ->
             mutableMapOf<String, Any?>().apply {
                 allFields.forEach { this[it] = null }
 
                 resultMap.forEach { (safeKey, list) ->
-                    val fieldNameMap = fieldNameMapping[safeKey]!!
+                    val fieldNameMap = fieldNameMapping[safeKey]
                     list.getOrNull(index)?.forEach { (key, value) ->
-                        this[fieldNameMap[key]!!] = value
+                        val fieldName = fieldNameMap?.get(key) ?: key
+                        this[fieldName] = value
                     }
                 }
             }

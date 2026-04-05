fcc2acaef7ed5d5b1f2f2b9d7d29f6e793163a3e
ousc <750154432@qq.com>
2026-04-05 10:29:14 +0800

    test(testing): add oneToMany cascade test data and relation query tests
    - Add BookDto for cascade query testing
    - Update Book and Chapter with cascade annotations
    - Add RelationQuery test cases

--- kronos-testing/src/test/kotlin/com/kotlinorm/beans/sample/oneToMany/BookDto.kt
+++ kronos-testing/src/test/kotlin/com/kotlinorm/beans/sample/oneToMany/BookDto.kt
@@ -1,4 +1,35 @@
+/**
+ * Copyright 2022-2025 kronos-orm
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kotlinorm.beans.sample.oneToMany
 
-class BookDto {
-}
+import com.kotlinorm.annotations.CreateTime
+import com.kotlinorm.annotations.DateTimeFormat
+import com.kotlinorm.annotations.LogicDelete
+import com.kotlinorm.annotations.PrimaryKey
+import com.kotlinorm.annotations.Table
+import com.kotlinorm.annotations.UpdateTime
+import com.kotlinorm.annotations.Version
+import com.kotlinorm.beans.sample.cascade.oneToMany.Chapter
+import com.kotlinorm.interfaces.KPojo
+import java.time.LocalDateTime
+
+@Table("tb_book")
+data class BookDto(
+    @PrimaryKey(identity = true)
+    val id: Int? = null,
+    val title: String? = null, // 书名
+) : KPojo
